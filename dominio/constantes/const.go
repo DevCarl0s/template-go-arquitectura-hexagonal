6c6be026d300aa12a1d2249fda3fe2c3c952681b
Carlos_DeLasSalas <carlos.salas@devitech.com.co>
2025-03-27 19:50:21 -0500

    puerto de bd

--- dominio/constantes/const.go
+++ dominio/constantes/const.go
@@ -3,7 +3,7 @@ package constantes
 var ID_POS int64 = 0
 
 const (
-	DB_CON      string = "postgres://pos_master:24002A8%C3%B1E5E588D09F41E3A230%C3%9101F7E8@localhost:8891/pos_transacciones?sslmode=allow"
+	DB_CON      string = "postgres://pos_master:24002A8%C3%B1E5E588D09F41E3A230%C3%9101F7E8@localhost:5432/pos_transacciones?sslmode=allow"
 	HOST_PORT   string = "10665"
 	HOST_IP     string = "0.0.0.0"
 	EFECTIVO    string = "EFECTIVO"
